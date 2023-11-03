0cc91da7497eb571ee4dc1a2f9ceb79453b48d8b
renan-leonel <renanleonelpro@gmail.com>
2023-11-03 15:45:13 -0300

    chore: upgraded to next v14

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withPWA = require('next-pwa')({
 	dest: 'public',
-	// disable: process.env.NODE_ENV === 'development',
+	disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
-	experimental: {
-		serverActions: true,
-	},
-});
+module.exports = withPWA({});
